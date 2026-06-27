208fa0c59d20dabe7dbee18ed3ca25268d588d01
acekkyus <graceangelinasilitonga@gmail.com>
2026-06-28 03:58:50 +0700

    Configure project for Render

--- sistem_informasi_olahraga_kesehatan/settings.py
+++ sistem_informasi_olahraga_kesehatan/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = 'django-insecure-change-this-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -23,11 +26,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
     'fitness',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -36,7 +43,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'sistem_informasi_olahraga_kesehatan.urls'
+
 
 TEMPLATES = [
     {
@@ -53,7 +62,11 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'sistem_informasi_olahraga_kesehatan.wsgi.application'
+
+
+# Database (SQLite sesuai ketentuan tugas)
 
 DATABASES = {
     'default': {
@@ -61,6 +74,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -77,6 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Internationalization
+
 LANGUAGE_CODE = 'id-id'
 
 TIME_ZONE = 'Asia/Jakarta'
@@ -85,6 +104,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Static files
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
